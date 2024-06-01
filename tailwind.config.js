dc4d84bc19f824d89215b6c6119836572dbb2aa2
sunbkk <paorat67@gmail.com>
2024-06-01 20:27:02 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// module.exports = {
+//     content: ["./**/*.html"],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+
+// ES module
+export default {
+    content: ["./**/*.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
